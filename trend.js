8092f2d40120cf1671ca2287a2e4a57f4a274412
LavanyaaCSE <142470176+LavanyaaCSE@users.noreply.github.com>
2024-03-28 23:06:28 +0530

    Add files via upload

---
+++ trend.js
@@ -0,0 +1,29 @@
+function downloadMusic() {
+  var musicURL = "C:/Users/chefa/Downloads/Indru Netru Naalai - Hiphop Tamizha - Copy.mp3";
+  var fileName = "song.mp3";
+  
+
+  var downloadedSongs = JSON.parse(localStorage.getItem("downloadedSongs")) || [];
+  downloadedSongs.push({ name: fileName, url: musicURL });
+  localStorage.setItem("downloadedSongs", JSON.stringify(downloadedSongs));
+
+  
+  updateDownloadSection();
+}
+
+function updateDownloadSection() {
+  var downloadList = document.getElementById("download-list");
+  var downloadedSongs = JSON.parse(localStorage.getItem("downloadedSongs")) || [];
+
+  
+  downloadList.innerHTML = "";
+
+  
+  downloadedSongs.forEach(function(song) {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = '<a href="' + song.url + '" download>' + song.name + '</a>';
+    downloadList.appendChild(listItem);
+  });
+}
+
+updateDownloadSection();
